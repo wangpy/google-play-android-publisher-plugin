7ed9b636ceec217996a62a804a2b6d5656114702
Christopher Orr <chris@orr.me.uk>
2019-11-20 23:23:05 +0100

    Increase connection timeouts as AAB uploads can be very large.
    The device-specific bundle delivered to phones can be up to 150MB, so
    the actual bundle uploaded to Google Play can be many times larger.
    
    Joe was getting timeout exceptions when uploading ~100MB bundle files,
    with the default timeout (which seems to be 20 seconds), and was seeing
    upload times of 2.5 minutes after fixing the timeout, so increasing the
    timeout will definitely be required (and it's presumably been a problem
    for users in the past, not that anybody has actually complained…).
    
    It's unclear what the read timeout means exactly; is it the amount of
    time before receiving an HTTP response (i.e. rather than being a TCP
    connection-level timeout), meaning that we don't "read" anything from
    the HTTP server while we're uploading a file?
    
    Anyway, now we have a one-hour read timeout, which I think should cover
    most people's bundle sizes and internet connection speeds.  I also think
    having a defined timeout is better than having an infinite timeout, as
    most Jenkins users probably don't have a job timeout configured.

--- src/main/java/org/jenkinsci/plugins/googleplayandroidpublisher/internal/UtilsImpl.java
+++ src/main/java/org/jenkinsci/plugins/googleplayandroidpublisher/internal/UtilsImpl.java
@@ -1,12 +1,14 @@
 package org.jenkinsci.plugins.googleplayandroidpublisher.internal;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.services.androidpublisher.AndroidPublisher;
 import com.google.jenkins.plugins.credentials.oauth.GoogleRobotCredentials;
-import java.security.GeneralSecurityException;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.googleplayandroidpublisher.AndroidPublisherScopeRequirement;
 import org.jenkinsci.plugins.googleplayandroidpublisher.Util;
+
+import java.security.GeneralSecurityException;
 
 public class UtilsImpl implements JenkinsUtil, AndroidUtil {
     private static UtilsImpl sInstance;
@@ -29,9 +31,19 @@ public class UtilsImpl implements JenkinsUtil, AndroidUtil {
     public AndroidPublisher createPublisherClient(GoogleRobotCredentials credentials, String pluginVersion)
             throws GeneralSecurityException {
         final Credential credential = credentials.getGoogleCredential(new AndroidPublisherScopeRequirement());
-        return new AndroidPublisher.Builder(credential.getTransport(), credential.getJsonFactory(), credential)
+        final HttpRequestInitializer requestInitializer = applyHttpConnectionTimeouts(credential);
+        return new AndroidPublisher.Builder(credential.getTransport(), credential.getJsonFactory(), requestInitializer)
                 .setApplicationName(String.format("Jenkins-GooglePlayAndroidPublisher/%s", pluginVersion))
                 .build();
+    }
+
+    // Adapted from https://developers.google.com/api-client-library/java/google-api-java-client/errors#timeouts
+    static HttpRequestInitializer applyHttpConnectionTimeouts(final HttpRequestInitializer delegate) {
+        return httpRequest -> {
+            delegate.initialize(httpRequest);
+            httpRequest.setConnectTimeout(30 * 1000);   // Allow more time for the initial connection
+            httpRequest.setReadTimeout(60 * 60 * 1000); // AABs can be huge, so allow for long upload requests
+        };
     }
 
     // endregion
